6525cf0b5f046e85a2e5cee4ac1b7178e9434f8e
Walton Ma <walton.ma@SJSU.edu>
2023-11-27 18:03:38 -0800

    Implemented Tailwind CSS Table w/ React

--- frontend/src/pages/Pay.jsx
+++ frontend/src/pages/Pay.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Card, Typography } from "@material-tailwind/react";
+
+const TABLE_HEAD = ["Past Salaries"]
 
 export default function Pay() {
 
@@ -17,13 +20,43 @@ export default function Pay() {
     }, []);
 
     return (
-        <div className="">
+        /* <div className="">
             {text.map((data) => (
                 <div>
                     Salary:  {data}
                 </div>
             )
             )}
-        </div>
+        </div> */
+        <Card className="h-full w-full overflow-scroll">
+            <table className="w-full min-w-max table-auto text-left">
+                <thead>
+                    <tr>
+                        {TABLE_HEAD.map((head) => (
+                        <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                            <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal leading-none opacity-70"
+                            >
+                            {head}
+                            </Typography>
+                        </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {text.map((data) => (
+                        <tr key={data} className="even:bg-blue-gray-50/50">
+                            <td className="p-4">
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                ${data}
+                                </Typography>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Card>
     ); 
 }
